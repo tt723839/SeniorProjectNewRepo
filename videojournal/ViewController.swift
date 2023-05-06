dab8e8d84533d293ab0c2679ce1e513614adc659
Tyler Thammavong <tylertham21@gmail.com>
2023-05-05 23:28:09 -0400

    Initial Commit

---
+++ videojournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  videojournal
+//
+//  Created by Tyler Thammavong on 5/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
